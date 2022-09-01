7d35275b4ce39d7e0c0c7f06cf1ad1bdda15084d
PURBASHIS BEHERA <purbashis01042000@gmail.com>
2022-09-01 15:15:56 +0530

    basic dictionary

---
+++ Dictionary.py
@@ -0,0 +1,27 @@
+'''
+Dictionary is a collection of key value pairs 
+
+Syntax: 
+a ={
+"key" : "value",
+"purbashis" : "gamer",
+"marks" : "100",
+"list" : [1,5,6,7,8,9,10,11,12]
+
+}
+'''
+myDict={
+
+"purbashis" : "gamer",
+"marks" : "100",
+"list" : [1,5,6,7,8,9,10,11,12],
+"anotherdict" : {'dantz' : 'daddy ',
+'nepal':'momo',
+'daddy':'mummy',
+'plugin':'json'
+}
+}
+
+print(myDict["purbashis"])
+print(myDict["list"])
+print(myDict["anotherdict"]['plugin'])
